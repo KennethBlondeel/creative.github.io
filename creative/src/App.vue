4c1a18af653dd04a8f9d7c4bfbc8be54f4536152
KennethBlondeel <72190963+KennethBlondeel@users.noreply.github.com>
2024-12-13 18:47:10 +0100

    scss

--- creative/src/App.vue
+++ creative/src/App.vue
@@ -1,41 +1,41 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+const config = {
+  itemsToShow: 1,
+  wrapAround: true,
+  autoplay: 3000,
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
-
-  <main></main>
+  <body>
+    <div>
+      <Carousel v-bind="config">
+        <Slide v-for="slide in 1" :key="slide">
+          <div class="carousel__item">
+            <img
+              src="https://elements-resized.envatousercontent.com/elements-cover-images/369775a9-ea89-44f5-aca1-4a3c4e3b4e4d?w=433&cf_fit=scale-down&q=85&format=auto&s=9f3b00f7ec57cbe4fb9859313c8ca6c02f8b8907a0e286a07e3466f7a44965ec"
+            />
+          </div>
+        </Slide>
+      </Carousel>
+    </div>
+  </body>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 header {
   line-height: 1.5;
 }
 
-.logo {
+body {
   display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  img {
+    width: 100%;
   }
 }
 </style>
